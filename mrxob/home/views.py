aaf594c0f08dba5499eae3afaf3350c7e42edcf9
김주언 <jooeon@gimjueon-ui-MacBookPro.local>
2021-04-12 09:38:04 +0900

    create basic home urls and views

--- mrxob/home/views.py
+++ mrxob/home/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("This is index page")
+
+
+def detail(request, room_id):
+    return HttpResponse(f"Specifications of the room #{room_id}.")
+
+
+def results(request, room_id):
+    response = f"furniture list in the room #{room_id}."
+    return HttpResponse(response)
+
+
+def vote(request, room_id):
+    return HttpResponse(f"user list of the room #{room_id}.")
